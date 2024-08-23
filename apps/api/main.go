edead593d236e58db33896126fe5159e603f12cc
Jason Gallavin <jasongallavin@gmail.com>
2024-08-22 22:47:54 -0400

    Move supertokens into its own file

--- apps/api/main.go
+++ apps/api/main.go
@@ -2,43 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
 	"github.com/supertokens/supertokens-golang/recipe/session"
 	"github.com/supertokens/supertokens-golang/supertokens"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	apiBasePath := "/auth"
-	websiteBasePath := "/auth"
-	err := supertokens.Init(supertokens.TypeInput{
-		Supertokens: &supertokens.ConnectionInfo{
-			// https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
-			ConnectionURI: "http://localhost:3567",
-			// APIKey: <API_KEY(if configured)>,
-		},
-		AppInfo: supertokens.AppInfo{
-			AppName:         "autoscaler",
-			APIDomain:       "http://localhost:8080",
-			WebsiteDomain:   "http://localhost:3000",
-			APIBasePath:     &apiBasePath,
-			WebsiteBasePath: &websiteBasePath,
-		},
-		RecipeList: []supertokens.Recipe{
-			emailpassword.Init(nil),
-			session.Init(nil),
-		},
-	})
-
+	err := InitSuperTokens()
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Print("Server running on port 4000")
-	//session.VerifySession(nil, likeCommentAPI)
-	//http.HandleFunc("/comment", likeCommentAPI)
-
-	err = http.ListenAndServe("localhost:4000", corsMiddleware(
+	err = http.ListenAndServe("localhost:4000", CorsMiddleware(
 		supertokens.Middleware(RouterMux())))
 	if err != nil {
 		panic(err.Error())
@@ -62,21 +37,4 @@ func likeCommentAPI(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 	//fmt.Println(userID)
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(response http.ResponseWriter, r *http.Request) {
-		response.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		response.Header().Set("Access-Control-Allow-Credentials", "true")
-		if r.Method == "OPTIONS" {
-			// we add content-type + other headers used by SuperTokens
-			response.Header().Set("Access-Control-Allow-Headers",
-				strings.Join(append([]string{"Content-Type"},
-					supertokens.GetAllCORSHeaders()...), ","))
-			response.Header().Set("Access-Control-Allow-Methods", "*")
-			response.Write([]byte(""))
-		} else {
-			next.ServeHTTP(response, r)
-		}
-	})
 }
---
+++ apps/api/supertokens.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/supertokens"
+	"net/http"
+	"strings"
+)
+
+func InitSuperTokens() error {
+	apiBasePath := "/auth"
+	websiteBasePath := "/auth"
+	err := supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			// https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+			ConnectionURI: "http://localhost:3567",
+			// APIKey: <API_KEY(if configured)>,
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:         "autoscaler",
+			APIDomain:       "http://localhost:8080",
+			WebsiteDomain:   "http://localhost:3000",
+			APIBasePath:     &apiBasePath,
+			WebsiteBasePath: &websiteBasePath,
+		},
+		RecipeList: []supertokens.Recipe{
+			emailpassword.Init(nil),
+			session.Init(nil),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(response http.ResponseWriter, r *http.Request) {
+		response.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		response.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			// we add content-type + other headers used by SuperTokens
+			response.Header().Set("Access-Control-Allow-Headers",
+				strings.Join(append([]string{"Content-Type"},
+					supertokens.GetAllCORSHeaders()...), ","))
+			response.Header().Set("Access-Control-Allow-Methods", "*")
+			response.Write([]byte(""))
+		} else {
+			next.ServeHTTP(response, r)
+		}
+	})
+}
