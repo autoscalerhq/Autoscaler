2b50bba22ecc16fda4ec8a83e4fb05dda8627455
Zac Clifton <43915749+Cliftonz@users.noreply.github.com>
2024-09-01 18:11:55 -0400

    Reorganize navigation components and update dependencies
    Renamed vertical-nav for better structure and integrated Nav component in layout. Updated packages including next and lucide-react to latest versions, enhancing performance and compatibility.

--- website/src/app/[org]/[env]/layout.tsx
+++ website/src/app/[org]/[env]/layout.tsx
@@ -1,193 +1,223 @@
-// app/layout.js
-
-import Link from 'next/link';
 import {Avatar, AvatarFallback, AvatarImage} from "~/components/ui/avatar";
 import {Button} from "~/components/ui/button";
+import {ReactNode} from "react";
+import {
+    Bell,
+    BookDashed,
+    BookUp, Boxes,
+    ChevronsLeftRight,
+    GitPullRequestDraft,
+    LucideLayoutDashboard,
+    PencilRuler,
+    Settings,
+    SquareDashedKanban,
+} from "lucide-react";
+import {Nav} from "~/components/navigation/vertical-nav";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel, DropdownMenuSeparator,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
     DropdownMenuTrigger
 } from "~/components/ui/dropdown-menu";
-import {ReactNode} from "react";
+import Feedback from "~/components/feedback/PageExperience";
+import {ModelSwitcher} from "~/components/navigation/model-switcher";
 
 
-export default function Layout({ children, params }: { children: ReactNode, params: {org: string, env: string}}) {
+export default function Layout({children, params}: { children: ReactNode, params: { org: string, env: string } }) {
 
     const base_link = `/${params.org}/${params.env}/`
 
     return (
-        <div className="flex " >
-            <nav className="w-64 bg-gray-100 p-4 h-screen overflow-auto">
-                <div className="flex flex-col justify-between h-full">
+        <div className="flex ">
+            <nav className="w-64 bg-gray-100 py-4 px-2 h-screen overflow-auto flex flex-col justify-between">
+                <div>
                     <div>
-                        <div>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button
-                                        className="flex items-center justify-between w-full"
-                                        aria-haspopup="menu"
-                                    >
-                              <span className="flex items-center">
-                                <Avatar>
-                                  <AvatarImage src="https://github.com/shadcn.png"/>
-                                  <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                bearbinary
-                              </span>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeWidth="2"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        >
-                                            <path d="m6 9 6 6 6-6"></path>
-                                        </svg>
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent>
-                                    <DropdownMenuLabel>Accounts</DropdownMenuLabel>
-                                    <DropdownMenuItem>bearbinary</DropdownMenuItem>
-                                    <DropdownMenuSeparator/>
-                                    <Link href="/join-account">
-                                        <DropdownMenuItem asChild>
-                                            <Button className="w-full justify-between">
-                                                <span>Create or join account</span>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="16"
-                                                    height="16"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                >
-                                                    <path d="M256 112v288m144-144H112"></path>
-                                                </svg>
-                                            </Button>
-                                        </DropdownMenuItem>
-                                    </Link>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                            <Link href={base_link + "settings"} passHref>
-                                <p>Settings</p>
-                            </Link>
-                            <Link href={base_link + "system-templates"} passHref>
-                                <p>System Templates</p>
-                            </Link>
-                            <Link href={base_link + "service-templates"} passHref>
-                                <p>Service Templates</p>
-                            </Link>
-                        </div>
-                        <div className="mt-4 flex-grow">
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button
-                                        className="flex items-center justify-between w-full"
-                                        aria-haspopup="menu"
-                                    >
-                              <span className="flex items-center">
-                                <Avatar>
-                                  <AvatarImage src="https://github.com/shadcn.png"/>
-                                  <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                bearbinary
-                              </span>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            stroke="currentColor"
-                                            strokeWidth="2"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                        >
-                                            <path d="m6 9 6 6 6-6"></path>
-                                        </svg>
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent>
-                                    <DropdownMenuLabel>Accounts</DropdownMenuLabel>
-                                    <DropdownMenuItem>bearbinary</DropdownMenuItem>
-                                    <DropdownMenuSeparator/>
-                                    <Link href="/join-account">
-                                        <DropdownMenuItem asChild>
-                                            <Button className="w-full justify-between">
-                                                <span>Create or join account</span>
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    width="16"
-                                                    height="16"
-                                                    viewBox="0 0 24 24"
-                                                    fill="none"
-                                                    stroke="currentColor"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                >
-                                                    <path d="M256 112v288m144-144H112"></path>
-                                                </svg>
-                                            </Button>
-                                        </DropdownMenuItem>
-                                    </Link>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
+                        {/*Project*/}
+                        <ModelSwitcher items={[
+                            {
+                                name: "Autoscaler",
+                                label: "Autoscaler",
+                                shortname: "prod"
+                            },
+                        ]} isCollapsed={false} ariaLabel={""} defaultItemName={"Autoscaler"}/>
 
-                            <Link href={base_link + "pull"} passHref>
-                                <p>Pull</p>
-                            </Link>
-                            <Link href={base_link + "stream"} passHref>
-                                <p>Stream</p>
-                            </Link>
-                            <Link href={base_link + "push"} passHref>
-                                <p>Push</p>
-                            </Link>
-                            <Link href={base_link + "policies"} passHref>
-                                <p>Polices</p>
-                            </Link>
-                            <Link href={base_link + "scalers"} passHref>
-                                <p>Scalers</p>
-                            </Link>
-                            <Link href={base_link + "analytics"} passHref>
-                                <p>Analytics</p>
-                            </Link>
-                            <Link href={base_link + "alerts"} passHref>
-                                <p>Alerts</p>
-                            </Link>
-                        </div>
+                        {/*<Separator title={"test"} className={"bg-gray-700 mt-1.5"}/>*/}
+
+                        <Nav
+                            isCollapsed={false}
+                            links={[
+                                {
+                                    title: "Settings",
+                                    href: base_link + "settings",
+                                    icon: Settings,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "System Templates",
+                                    href: base_link + "system-templates",
+                                    icon: BookDashed,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Service Templates",
+                                    href: base_link + "service-templates",
+                                    icon: SquareDashedKanban,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Search",
+                                    href: "",
+                                    label: "CMD + K",
+                                    icon: SquareDashedKanban,
+                                    variant: "ghost",
+                                },
+                            ]}
+                        />
                     </div>
 
-                    <div className={"h-full"}>
-                        <Link href={base_link + "get-started"} passHref>
-                            <p>Get started</p>
-                        </Link>
-                        <Link href="https://docs.autoscaler.dev" passHref>
-                            <p>Docs</p>
-                        </Link>
+                    {/*<Separator title={"test"} className={"bg-gray-700"}/>*/}
+
+                    <div className="mt-4">
+                        {/*Env*/}
+                        <ModelSwitcher
+                            defaultItemName={"Development"}
+                            items={[
+                                {
+                                    name: "Production",
+                                    label: "Prod",
+                                    icon: <GitPullRequestDraft/>,
+                                    shortname: "prod"
+                                },
+                                {
+                                    name: "Development",
+                                    label: "Dev",
+                                    shortname: "dev"
+                                },
+                            ]}
+                            isCollapsed={false}
+                         ariaLabel={""}/>
+
+                        <ModelSwitcher
+                            defaultItemName={"Api"}
+                            items={[
+                                {
+                                    name: "Api",
+                                    label: "Api",
+                                    shortname: "api"
+                                },
+                                {
+                                    name: "Webhook",
+                                    label: "Wh",
+                                    shortname: "wh"
+                                },
+                            ]}
+                            isCollapsed={false}
+                            ariaLabel={""}
+                            className={"mt-4"}
+                        />
+
+                        <Nav
+                            isCollapsed={false}
+                            links={[
+                                {
+                                    title: "Overview",
+                                    href: base_link + "overview",
+                                    icon: Boxes,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Pull",
+                                    href: base_link + "pull",
+                                    icon: GitPullRequestDraft,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Streaming",
+                                    href: base_link + "stream",
+                                    icon: ChevronsLeftRight,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Push",
+                                    href: base_link + "push",
+                                    icon: BookUp,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Polices",
+                                    href: base_link + "policies",
+                                    icon: PencilRuler,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Analytics",
+                                    href: base_link + "analytics",
+                                    icon: LucideLayoutDashboard,
+                                    variant: "ghost",
+                                },
+                                {
+                                    title: "Alerts",
+                                    href: base_link + "alerts",
+                                    icon: Bell,
+                                    variant: "ghost",
+                                },
+                            ]}
+                        />
                     </div>
+                </div>
+
+                <div className="mt-auto">
+                    <Nav
+                        isCollapsed={false}
+                        links={[
+                            {
+                                title: "Get started",
+                                href: base_link + "get-started",
+                                icon: Settings,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Docs",
+                                href: "https://docs.autoscaler.dev",
+                                icon: Settings,
+                                variant: "ghost",
+                            },
+                        ]}
+                    />
                 </div>
             </nav>
-            <div className="flex flex-col w-full">
-                <div className="w-full flex space-x-10 items-center justify-end pt-2 border-b border-gray-400">
-                    <Button className="btn-primary">
-                        Feedback?
-                    </Button>
 
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png"/>
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </div>
+            <div className="flex flex-col w-full">
+                <header
+                    className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background px-4 border-gray-200">
+
+                    <Feedback className={"ml-auto"}/>
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="overflow-hidden rounded-full"
+                            >
+                                <Avatar>
+                                    <AvatarImage src="https://github.com/shadcn.png"/>
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator/>
+                            <DropdownMenuItem>Settings</DropdownMenuItem>
+                            <DropdownMenuItem>Support</DropdownMenuItem>
+                            <DropdownMenuSeparator/>
+                            <DropdownMenuItem>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </header>
 
                 <main className="flex-1 p-4 overflow-auto min-w-full ">
                     {children}
