55547d53dd23c4b84ed8bfc962f2a2c14e90141f
Zac Clifton <43915749+Cliftonz@users.noreply.github.com>
2024-09-08 16:07:23 -0400

    Enhance keyboard navigation and add Kbd component
    Refactored keyboard navigation logic for consistency and clarity. Added a new `Kbd` component for displaying keyboard shortcuts within the UI. Updated the `CommandDialog` to use this new component and incorporated selected state handling for improved user experience.

--- website/src/components/navigation/command-context.tsx
+++ website/src/components/navigation/command-context.tsx
@@ -1,10 +1,13 @@
 "use client"
+
 import React, { useState, useEffect, createContext, ReactNode, useContext } from "react";
 import {useRouter} from "next/navigation";
 
 interface CommandContextProps {
     open: boolean;
     setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    selected: string;
+    setSelected: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const CommandContext = createContext<CommandContextProps | undefined>(undefined);
@@ -16,7 +19,9 @@ interface ICommanderProvider {
 }
 
 export const CommandProvider = ({ children, org, env }: ICommanderProvider ) => {
+
     const [open, setOpen] = useState(false);
+    const [selected, setSelected] = useState<string>("");
 
     const router = useRouter();
 
@@ -24,61 +29,75 @@ export const CommandProvider = ({ children, org, env }: ICommanderProvider ) => 
     const base_link = `/${org}/${env}/`;
     const settings_link = `/${org}/settings/`;
 
+    function ActOnValue(e: KeyboardEvent, val: string){
+        switch (val) {
+            case "j":
+                e.preventDefault();
+                setOpen((open) => !open);
+                break;
+            case "r":
+                e.preventDefault();
+                // Add your logic to open Profile
+                console.log("Open Profile");
+                // router.push('/profile');
+                setOpen(false);
+                break;
+            case "b":
+                e.preventDefault();
+                router.push(settings_link+'billing');
+                setOpen(false);
+                break;
+            case "e":
+                e.preventDefault();
+                router.push(settings_link+'environments');
+                setOpen(false);
+                break;
+            case "l":
+                e.preventDefault();
+                router.push(settings_link+'audit-logs');
+                setOpen(false);
+                break;
+            case "t":
+                e.preventDefault();
+                router.push(settings_link+'access-tokens');
+                setOpen(false);
+                break;
+            case "m":
+                e.preventDefault();
+                router.push(settings_link+'members');
+                setOpen(false);
+                break;
+            case "delete":
+            case "backspace":
+                e.preventDefault();
+                router.back();
+                break;
+        }
+    }
+
     useEffect(() => {
         const down = (e: KeyboardEvent) => {
-            if (e.metaKey || e.ctrlKey) {
-                switch (e.key.toLowerCase()) {
-                    case "j":
-                        e.preventDefault();
-                        setOpen((open) => !open);
-                        break;
-                    case "r":
-                        e.preventDefault();
-                        // Add your logic to open Profile
-                        console.log("Open Profile");
-                        // router.push('/profile');
-                        setOpen(false);
-                        break;
-                    case "b":
-                        e.preventDefault();
-                        router.push(settings_link+'billing');
-                        setOpen(false);
-                        break;
-                    case "e":
-                        e.preventDefault();
-                        router.push(settings_link+'environments');
-                        setOpen(false);
-                        break;
-                    case "l":
-                        e.preventDefault();
-                        router.push(settings_link+'audit-logs');
-                        setOpen(false);
-                        break;
-                    case "t":
-                        e.preventDefault();
-                        router.push(settings_link+'access-tokens');
-                        setOpen(false);
-                        break;
-                    case "m":
-                        e.preventDefault();
-                        router.push(settings_link+'members');
-                        setOpen(false);
-                        break;
-                    case "delete":
-                    case "backspace":
-                        e.preventDefault();
-                        router.back();
-                        break;
-                }
+
+            console.log("selected dialog", selected)
+
+            if (e.key === "Enter" && selected) {
+                console.log("selected: ", selected)
+                ActOnValue(e, selected)
+                setOpen(false);
             }
+
+            if (e.metaKey || e.ctrlKey) {
+                ActOnValue(e, e.key.toLowerCase())
+            }
+
         };
 
         document.addEventListener("keydown", down);
         return () => document.removeEventListener("keydown", down);
-    }, []);
+    }, [open, selected]);
 
     return (
-        <CommandContext.Provider value={{ open, setOpen }}>
+        <CommandContext.Provider value={{ open, setOpen , selected, setSelected }}>
             {children}
         </CommandContext.Provider>
     );
--- website/src/components/navigation/command-dialog.tsx
+++ website/src/components/navigation/command-dialog.tsx
@@ -7,12 +7,9 @@ import {
     User,
     ArrowDownUp,
     Bell,
-    BookUp,
     Boxes,
-    ChevronsLeftRight,
     GitPullRequestDraft,
     PencilRuler,
-    SquareLibrary
 } from "lucide-react"
 
 import {
@@ -26,15 +23,22 @@ import {
     CommandShortcut,
 } from "~/components/ui/command"
 
-import { useCommand } from "~/components/navigation/command-context"
+import { useCommand } from "~/components/navigation/command-context";
+import {DialogFooter} from "~/components/ui/dialog";
+import {Kbd} from "~/components/ui/kbd";
 
 export function Command() {
 
-    const { open, setOpen } = useCommand();
+    const { open, setOpen, selected, setSelected } = useCommand();
 
     return (
         <>
-            <CommandDialog open={open} onOpenChange={setOpen} title={"test"}>
+            <CommandDialog open={open} onOpenChange={ (open) => {
+                setOpen(open)
+                if(!open) {
+                    setSelected("")
+                }
+            }} title={"test"} >
 
                 <CommandInput placeholder="Type a command or search..." onClick={() => {console.log("input click")}}/>
 
@@ -43,66 +47,98 @@ export function Command() {
                     <CommandEmpty>No results found.</CommandEmpty>
 
                     <CommandGroup heading="Settings">
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("r") }}
+                        >
                             <User className="mr-2 h-4 w-4" />
                             <span>Profile</span>
-                            <CommandShortcut>⌘ + R</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd>  <Kbd>r</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("b") }}
+                        >
                             <CreditCard className="mr-2 h-4 w-4" />
                             <span>Billing</span>
-                            <CommandShortcut>⌘ + B</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>b</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("e") }}
+                        >
                             <Settings className="mr-2 h-4 w-4" />
                             <span>Environment</span>
-                            <CommandShortcut>⌘ + E</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>e</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("l") }}
+                        >
                             <Settings className="mr-2 h-4 w-4" />
                             <span>Audit Log</span>
-                            <CommandShortcut>⌘ + L</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>l</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("t") }}
+                        >
                             <Settings className="mr-2 h-4 w-4" />
                             <span>Access Tokens</span>
-                            <CommandShortcut>⌘ + T</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>t</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("m") }}
+                        >
                             <Settings className="mr-2 h-4 w-4" />
                             <span>Members</span>
-                            <CommandShortcut>⌘ + M</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>m</Kbd></CommandShortcut>
                         </CommandItem>
                     </CommandGroup>
                     <CommandSeparator />
                     <CommandGroup heading="Env Links">
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("1") }}
+                        >
                             <Boxes className="mr-2 h-4 w-4" />
                             <span>Overview</span>
-                            <CommandShortcut>⌘ + 1</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>1</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("2") }}
+                        >
                             <GitPullRequestDraft className="mr-2 h-4 w-4" />
                             <span>Inputs</span>
-                            <CommandShortcut>⌘ + 2</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>2</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("3") }}
+                        >
                             <PencilRuler className="mr-2 h-4 w-4" />
                             <span>Polices</span>
-                            <CommandShortcut>⌘ + 3</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>3</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("4") }}
+                        >
                             <ArrowDownUp className="mr-2 h-4 w-4" />
                             <span>Scalers</span>
-                            <CommandShortcut>⌘ + 4</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd> <Kbd>4</Kbd></CommandShortcut>
                         </CommandItem>
-                        <CommandItem>
+                        <CommandItem
+                            onSelect={(value) => {setSelected("5") }}
+                        >
                             <Bell className="mr-2 h-4 w-4" />
                             <CommandShortcut>Actions</CommandShortcut>
-                            <CommandShortcut>⌘ + 5</CommandShortcut>
+                            <CommandShortcut><Kbd>⌘</Kbd>  <Kbd>5</Kbd></CommandShortcut>
                         </CommandItem>
                     </CommandGroup>
                 </CommandList>
+
+                <DialogFooter className="flex flex-col text-sm border border-t-1 ">
+
+                    <div className="flex items-center m-2 text-sm text-gray-500 mr-4">
+                        <span>Go to</span>
+                        <Kbd className={"ml-2 text-bold text-md"}>&#x23CE;</Kbd>
+                    </div>
+
+                </DialogFooter>
+
             </CommandDialog>
         </>
     );
