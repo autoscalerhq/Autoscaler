b92f50d3a6f12bc14e0ffc8ba4cf68bfb275af57
Jason Gallavin <jasongallavin@gmail.com>
2024-09-12 18:31:41 -0400

    Remove the web folder

--- apps/web/src/app/config/supertokens/appInfo.ts
+++
@@ -1,7 +0,0 @@
-export const appInfo = {
-  appName: "AutoScaler",
-  apiDomain: "http://localhost:4000",
-  websiteDomain: "http://localhost:3000",
-  apiBasePath: "/auth",
-  websiteBasePath: "/auth",
-};
--- apps/web/src/app/server-page.tsx
+++
@@ -1,8 +0,0 @@
-
-export function ServerPage() {
-  return (
-    <div>
-      <h1>Welcome to server-page!</h1>
-    </div>
-  );
-}
