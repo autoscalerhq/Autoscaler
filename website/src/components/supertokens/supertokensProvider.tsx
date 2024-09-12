4cdf028b8044b27cbefe4e9e9652dcef5f62bfbe
Jason Gallavin <jasongallavin@gmail.com>
2024-09-12 19:45:15 -0400

    Consolidate call to Supertokens init

--- website/src/components/supertokens/supertokensProvider.tsx
+++ website/src/components/supertokens/supertokensProvider.tsx
@@ -1,13 +1,12 @@
 "use client";
 import React from "react";
 import { SuperTokensWrapper } from "supertokens-auth-react";
-import SuperTokensReact from "supertokens-auth-react";
-import { frontendConfig, setRouter } from "../../config/supertokens/frontend";
+import {ensureFrontendSuperTokensInit, setRouter} from '~/config/supertokens/frontend';
 import { usePathname, useRouter } from "next/navigation";
 
 if (typeof window !== 'undefined') {
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
-  SuperTokensReact.init(frontendConfig());
+  ensureFrontendSuperTokensInit();
 }
 
 export const SuperTokensProvider: React.FC<React.PropsWithChildren<{}>> = ({
