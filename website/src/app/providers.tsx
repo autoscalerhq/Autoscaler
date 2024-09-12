d13f756342ac2d04bf0b99823478381a1f20b4e8
Jason Gallavin <jasongallavin@gmail.com>
2024-09-12 19:45:55 -0400

    Remove duplicate call to SuperTokens.init with the wrong settings that caused refreshing the token to fail

--- website/src/app/providers.tsx
+++ website/src/app/providers.tsx
@@ -3,9 +3,6 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import {env} from "~/env";
 import {ReactNode} from "react";
-import SuperTokens from "supertokens-auth-react";
-import ThirdParty, {Apple, Facebook, Github, Google} from "supertokens-auth-react/recipe/thirdparty";
-import Session from "supertokens-auth-react/recipe/session";
 
 if (typeof window !== 'undefined' && env.NEXT_PUBLIC_POSTHOG_KEY ) {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
@@ -21,30 +18,5 @@ export function CSPostHogProvider({ children }: { children: ReactNode }) {
     )
 }
 
-if (typeof window !== 'undefined'  ) {
-    SuperTokens.init({
-        appInfo: {
-            appName: "autoscaler",
-            apiDomain: "http://localhost:8080",
-            websiteDomain: "http://localhost:3000",
-            apiBasePath: "/auth",
-            websiteBasePath: "/auth"
-        },
-        recipeList: [
-            ThirdParty.init({
-                signInAndUpFeature: {
-                    providers: [
-                        Github.init(),
-                        Google.init(),
-                        Facebook.init(),
-                        Apple.init(),
-                    ]
-                }
-            }),
-            Session.init()
-        ]
-    });
-
-}
 
 
