37a6445dcecacebc0e17066c4b85703eb2679835
Jason Gallavin <jasongallavin@gmail.com>
2024-09-11 21:49:24 -0400

    Move auth wrapper to the layout file

--- website/src/app/(protected)/app/page.tsx
+++ website/src/app/(protected)/app/page.tsx
@@ -1,27 +1,15 @@
 import {HomeComponent} from '~/components/auth/home';
 import styles from "./page.module.css";
-import {AuthWrapper} from '~/components/supertokens/SuperTokensAuthWrapper';
 import {useApiOnServer} from '~/api-client/server-hooks';
 
 export default async function Home() {
-  return (
-    <AuthWrapper>
-     <HomeInner />
-    </AuthWrapper>
-  );
-}
-
-async function HomeInner() {
-   const api = useApiOnServer();
+  const api = useApiOnServer();
   const comment = await api.getComment()
   return (
-    <AuthWrapper>
       <main className={styles.main}>
         <span>{comment}</span>
         <HomeComponent />
       </main>
-    </AuthWrapper>
   );
 }
-
 
--- website/src/app/auth/[[...path]]/page.tsx
+++ website/src/app/auth/[[...path]]/page.tsx
@@ -9,8 +9,8 @@ export default function Auth() {
   // if the user visits a page that is not handled by us (like /auth/random), then we redirect them back to the auth page.
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    if (SuperTokens.canHandleRoute(PreBuiltUIList) === false) {
-      redirectToAuth({ redirectBack: false });
+    if (!SuperTokens.canHandleRoute(PreBuiltUIList)) {
+      void redirectToAuth({ redirectBack: false });
     } else {
       setLoaded(true);
     }
