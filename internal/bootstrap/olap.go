d861a062fc9ad1569913fc0890504daa9f27341c
Zac Clifton <43915749+Cliftonz@users.noreply.github.com>
2024-09-20 11:19:53 -0400

    Rename gorm.go to db.go and add OLAP DB singleton
    Renamed gorm.go to db.go for clarity and consistency. Added GetOLAPDBInstance function in olap.go to create a singleton GORM database connection for OLAP purposes.

--- internal/bootstrap/olap.go
+++ internal/bootstrap/olap.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 	"sync"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/clickhouse"
 	"gorm.io/gorm"
 )
 
-var dmLock = &sync.Mutex{}
-var dbInstance *gorm.DB
+var olapDBLock = &sync.Mutex{}
+var olapDbInstance *gorm.DB
 
-// getDBInstance returns a singleton instance of a GORM database connection
-func getDBInstance() (*gorm.DB, error) {
+// GetOLAPDBInstance returns a singleton instance of a GORM database connection for an OLAP database
+func GetOLAPDBInstance() (*gorm.DB, error) {
 	if dbInstance == nil {
-		dmLock.Lock()
-		defer dmLock.Unlock()
+		olapDBLock.Lock()
+		defer olapDBLock.Unlock()
 		if dbInstance == nil {
 			fmt.Println("Creating single GORM database instance now.")
 			dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
-			db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+			db, err := gorm.Open(clickhouse.Open(dsn), &gorm.Config{})
 			if err != nil {
 				return nil, fmt.Errorf("failed to connect to database: %w", err)
 			}
-			dbInstance = db
+			olapDbInstance = db
 		} else {
 			fmt.Println("GORM database instance already created.")
 		}
@@ -31,5 +31,5 @@ func getDBInstance() (*gorm.DB, error) {
 		fmt.Println("GORM database instance already created.")
 	}
 
-	return dbInstance, nil
+	return olapDbInstance, nil
 }
